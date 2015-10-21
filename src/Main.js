f802a270a5317344f399dcf166e978431c1c2dd7
kRITZCREEK <christoph.hegemann@gmx.net>
2015-10-21 19:55:26 +0200

    some additions

--- src/Main.js
+++ src/Main.js
@@ -6,6 +6,10 @@ var users = [
   { name: 'Lissy', age: null }
 ];
 
+var Team = function(x,y) {
+    return {x: x, y: y};
+}
+
 var findUser = function(userId, cb, err){
   if(userId < users.length){
     cb(users[userId]);
@@ -13,8 +17,40 @@ var findUser = function(userId, cb, err){
     err("User not found");
   }
 };
+module.exports.findUser = findUser;
 
-module.exports.findUserJS = findUser;
+// var buildTeam = function(idx, idy, cb, err){
+//     findUser(idx, function(x){
+//         findUser(idy, function(y){
+//             cb(new Team(x,y));
+//         }, err)
+//     }, err);
+// };
+// module.exports.buildTeam = buildTeam;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Wrapper für PureScript
 module.exports.findUserPS = function(userId, cb, err){
   return function(){
     findUser(
